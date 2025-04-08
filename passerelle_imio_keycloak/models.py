ddbc4dcb070c29af62c82e7f599751f041dea4d3
vboxuser <vboxuser@vbox>
2025-04-08 15:35:43 +0200

    add endpoints users

--- passerelle_imio_keycloak/models.py
+++ passerelle_imio_keycloak/models.py
@@ -45,25 +45,95 @@ class KeycloakConnector(BaseResource):
     class Meta:
         verbose_name = "Connecteur Keycloak"
 
-    @property
-    def session(self):
-        session = requests.Session()
-        session.headers.update({'Content-Type': 'application/x-www-form-urlencoded'})
-        return session
-
     @endpoint(
-        methods=["post"],
+        methods=["get"],
         name="get-bearer-token",
         perm="can_access",
         description="Récupérer le Bearer Token",
         long_description="Récupérer le Bearer Token.",
-        display_order=0,
-        display_category="Test",
+        display_order=1,
+        display_category="Access",
     )
-    def test(self, request):
+    def access_token(self, request):
         url = f"{self.url}realms/master/protocol/openid-connect/token"  # Url et endpoint à contacter
         payload = f'client_id={self.client_id}&password={self.password}&grant_type=password&username={self.username}'
-        header = {'Content-Type': 'application/x-www-form-urlencoded'}
-        r = requests.post(url=url, header=header, data=payload)
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        r = requests.post(url=url, headers=headers, data=payload)
         access_token = r.json()["access_token"]
         return {"access_token": access_token}
+    
+    @endpoint(
+        methods=["get"],
+        name="read-users",
+        perm='can_access',
+        description="Récupérer la liste des users d'un realm",
+        long_description="Récupérer la liste des users pour un realm donné",
+        display_order=1,
+        display_category="User",
+        example_pattern="users/",
+        pattern="^users/$",
+        parameters={
+            "realm": {
+                "description": "Tenant Keycloak/Collectivité",
+                "example_value": "imio",
+            }
+        }
+    )
+    def get_users(self, request, realm):
+        url = f"{self.url}admin/realms/{realm}/users"  # Url et endpoint à contacter
+        token = self.access_token(request)["access_token"]
+        headers = {"Authorization": "Bearer " + token}
+        r = requests.get(url=url, headers=headers)
+        return {"data": r.json()}
+
+    @endpoint(
+        methods=["get"],
+        name="read-user-groups",
+        perm='can_access',
+        description="Récupérer la liste de groupe d'un user",
+        long_description="Récupérer la liste de groupe d'un user",
+        display_order=2,
+        display_category="User",
+        parameters={
+            "realm": {
+                "description": "Tenant Keycloak/Collectivité",
+                "example_value": "imio",
+            },
+            "user_id": {
+                "description": "GUID de l'utilisateur",
+                "example_value": "4d49f2eb-890d-47e9-8cb4-3910fc17b66b",
+            }
+        }
+    )
+    def get_user_groups(self, request, realm, user_id):
+        url = f"{self.url}admin/realms/{realm}/users/{user_id}/groups"  # Url et endpoint à contacter
+        token = self.access_token(request)["access_token"]
+        headers = {"Authorization": "Bearer " + token}
+        r = requests.get(url=url, headers=headers)
+        return {"data": r.json()}
+
+    @endpoint(
+        methods=["get"],
+        name="read-user-credentials",
+        perm='can_access',
+        description="Récupérer les types de connection d'un utilisateur",
+        long_description="Récupérer les types de connection d'un utilisateur",
+        display_order=3,
+        display_category="User",
+        parameters={
+            "realm": {
+                "description": "Tenant Keycloak/Collectivité",
+                "example_value": "imio",
+            },
+            "user_id": {
+                "description": "GUID de l'utilisateur",
+                "example_value": "4d49f2eb-890d-47e9-8cb4-3910fc17b66b",
+            }
+        }
+    )
+    def get_user_credentials(self, request, realm, user_id):
+        url = f"{self.url}admin/realms/{realm}/users/{user_id}/credentials"  # Url et endpoint à contacter
+        token = self.access_token(request)["access_token"]
+        headers = {"Authorization": "Bearer " + token}
+        r = requests.get(url=url, headers=headers)
+        return {"data": r.json()}
